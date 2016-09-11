a9dc67cf3eaf81c4a22310d22642688fffad3765
Gy0m <landreaug@gmail.com>
2016-09-11 21:22:28 +0200

    add project to git-hub

---
+++ jquery.js
@@ -0,0 +1,30 @@
+$('h1').on('mouseover', function(){
+	$('h1').css('color', 'red');
+$('h1').on('mouseleave', function(){
+	$('h1').css('color', '#ffe900');
+	});
+});
+$('li a:eq(0)').on('mouseover', function(){
+	$('li a:eq(0)').css('color', 'White');
+$('li a:eq(0)').on('mouseleave', function(){
+	$('li a:eq(0)').css('color', 'black');
+	});
+});
+$('li a:eq(1)').on('mouseover', function(){
+	$('li a:eq(1)').css('color', 'White');
+$('li a:eq(1)').on('mouseleave', function(){
+	$('li a:eq(1)').css('color', 'black');
+	});
+});
+$('li a:eq(2)').on('mouseover', function(){
+	$('li a:eq(2)').css('color', 'White');
+$('li a:eq(2)').on('mouseleave', function(){
+	$('li a:eq(2)').css('color', 'black');
+	});
+});
+$('li a:eq(3)').on('mouseover', function(){
+	$('li a:eq(3)').css('color', 'White');
+$('li a:eq(3)').on('mouseleave', function(){
+	$('li a:eq(3)').css('color', 'black');
+	});
+});
